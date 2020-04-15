da13a36a5612e1525bcb266ffa0ea656eb7d50b1
Cerve <59832296+cerver1@users.noreply.github.com>
2020-04-14 22:41:32 -0400

    Twinkle little star commit

---
+++ stringSimpleFormat.py
@@ -0,0 +1,9 @@
+
+#Newline
+
+print("Twinkle, twinkle, little star,") 
+print("\tHow I wonder what you are!") 
+print("\t\tUp above the world so high,") 
+print("\t\tLike a diamond in the sky.") 
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
