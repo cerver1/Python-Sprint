5a0e2ee1f44bc0593a3a16b0e5eb2fd653fcc65f
Cerve <59832296+cerver1@users.noreply.github.com>
2020-06-23 19:10:37 -0400

    dataReader

---
+++ dataReaderPandas.py
@@ -0,0 +1,12 @@
+import pandas_datareader.data as web
+import datetime
+
+start = datetime.datetime(2015, 1,1)
+end = datetime.datetime(2017,1,1)
+
+facebook = web.DataReader('FB','google', start, end)
+
+facebook.head().show()
+
+
+# throws an exception
